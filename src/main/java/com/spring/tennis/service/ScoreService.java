955698a4542944b61a40583d9cdd528a0ae9ee87
Thejashree <thejashreehk@gmail.com>
2023-02-22 20:06:10 +0530

    feat: logic for equal score and deuce

--- src/main/java/com/spring/tennis/service/ScoreService.java
+++ src/main/java/com/spring/tennis/service/ScoreService.java
@@ -5,12 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScoreService {
 
+    private static final String ALL = "All";
+    private static final String DEUCE = "Deuce";
+    private static final int TWO = 2;
+
     public String getScore(int playerOneScore, int playerTwoScore) {
+        if (playerOneScore == playerTwoScore) {
+            return playerOneScore > TWO ? DEUCE : translateScore(playerOneScore) + " " + ALL;
+        }
         return String.format("%s, %s", translateScore(playerOneScore), translateScore(playerTwoScore));
     }
 
-    public String translateScore(int score){
-        switch(score){
+    public String translateScore(int score) {
+        switch (score) {
             case 3:
                 return "Forty";
             case 2:
--- src/test/java/com/spring/tennis/service/ScoreServiceTest.java
+++ src/test/java/com/spring/tennis/service/ScoreServiceTest.java
@@ -1,7 +1,8 @@
 package com.spring.tennis.service;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -13,10 +14,16 @@ public class ScoreServiceTest {
     @InjectMocks
     ScoreService scoreService;
 
-    @Test
-    public void testGetScore() {
-        String translatedScore = scoreService.getScore(1, 2);
-        assertEquals("Fifteen, Thirty", translatedScore);
+    @ParameterizedTest
+    @CsvSource({
+            "0,0,Love All",
+            "1,1,Fifteen All",
+            "2,2,Thirty All",
+            "3,3,Deuce",
+    })
+    public void testGetScore(int playerOnePoints, int playerTwoPoints, String expectedScore) {
+        String translatedScore = scoreService.getScore(playerOnePoints, playerTwoPoints);
+        assertEquals(expectedScore, translatedScore);
     }
 
 }
