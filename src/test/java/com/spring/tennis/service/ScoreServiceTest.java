2a1b8f879442303db4d23ddfef39d72b26c24eff
Thejashree <thejashreehk@gmail.com>
2023-02-22 21:43:21 +0530

    feat: test getScore with more combination of scores

--- src/test/java/com/spring/tennis/service/ScoreServiceTest.java
+++ src/test/java/com/spring/tennis/service/ScoreServiceTest.java
@@ -24,6 +24,22 @@ public class ScoreServiceTest {
             "4,3,PlayerOne Advantage",
             "4,6,PlayerTwo Wins",
             "6,4,PlayerOne Wins",
+            "0,1,Love Fifteen",
+            "0,2,Love Thirty",
+            "0,3,Love Forty",
+            "1,0,Fifteen Love",
+            "2,0,Thirty Love",
+            "3,0,Forty Love",
+            "1,2,Fifteen Thirty",
+            "1,3,Fifteen Forty",
+            "2,1,Thirty Fifteen",
+            "3,1,Forty Fifteen",
+            "2,3,Thirty Forty",
+            "3,2,Forty Thirty",
+            "3,5,PlayerTwo Wins",
+            "4,0,PlayerOne Wins",
+            "4,1,PlayerOne Wins",
+            "4,2,PlayerOne Wins",
     })
     public void testGetScore(int playerOnePoints, int playerTwoPoints, String expectedScore) {
         String translatedScore = scoreService.getScore(playerOnePoints, playerTwoPoints);
