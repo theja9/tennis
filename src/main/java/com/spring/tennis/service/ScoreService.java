9714b0648402301bed2fec043b8e281b56737bb6
Thejashree <thejashreehk@gmail.com>
2023-02-22 21:15:09 +0530

    refactor: getScore logic refactor and replace strings with constants

--- src/main/java/com/spring/tennis/service/ScoreService.java
+++ src/main/java/com/spring/tennis/service/ScoreService.java
@@ -11,41 +11,44 @@ public class ScoreService {
     private static final String WINS = "Wins";
     private static final String PLAYER_ONE = "PlayerOne";
     private static final String PLAYER_TWO = "PlayerTwo";
+    private static final int ZERO = 0;
     private static final int ONE = 1;
     private static final int TWO = 2;
     private static final int THREE = 3;
     private static final String SPACE = " ";
+    private static final String LOVE = "Love";
+    private static final String FIFTEEN = "Fifteen";
+    private static final String THIRTY = "Thirty";
+    private static final String FORTY = "Forty";
 
     public String getScore(int playerOneScore, int playerTwoScore) {
         if (playerOneScore == playerTwoScore) {
             return playerOneScore > TWO ? DEUCE : translateScore(playerOneScore) + SPACE + ALL;
         } else if (Math.max(playerOneScore, playerTwoScore) > THREE) {
-            if (playerOneScore == playerTwoScore + ONE) {
-                return PLAYER_ONE + SPACE + ADVANTAGE;
-            }
-            if (playerTwoScore == playerOneScore + ONE) {
-                return PLAYER_TWO + SPACE + ADVANTAGE;
-            }
-            if (playerOneScore == playerTwoScore + TWO) {
-                return PLAYER_ONE + SPACE + WINS;
-            }
-            if (playerTwoScore == playerOneScore + TWO) {
-                return PLAYER_TWO + SPACE + WINS;
-            }
+            String score = isPointDifferenceOne(playerOneScore, playerTwoScore) ? ADVANTAGE : WINS;
+            return getHighestScorer(playerOneScore, playerTwoScore) + SPACE + score;
         }
         return String.format("%s, %s", translateScore(playerOneScore), translateScore(playerTwoScore));
     }
 
+    private boolean isPointDifferenceOne(int playerOneScore, int playerTwoScore) {
+        return Math.abs(playerOneScore - playerTwoScore) == ONE;
+    }
+
+    private String getHighestScorer(int playerOneScore, int playerTwoScore) {
+        return playerOneScore > playerTwoScore ? PLAYER_ONE : PLAYER_TWO;
+    }
+
     public String translateScore(int score) {
         switch (score) {
-            case 3:
-                return "Forty";
-            case 2:
-                return "Thirty";
-            case 1:
-                return "Fifteen";
-            case 0:
-                return "Love";
+            case THREE:
+                return FORTY;
+            case TWO:
+                return THIRTY;
+            case ONE:
+                return FIFTEEN;
+            case ZERO:
+                return LOVE;
         }
         throw new IllegalArgumentException("Illegal score: " + score);
     }
